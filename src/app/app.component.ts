810dfe6595de2e0a2b84700ea5f5d768efb6fa58
deejayy <deejayy@deejayy.hu>
2023-12-02 17:13:17 +0100

    Finalize working app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { AppModule } from './app.module';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, AppModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,47 @@
 // eslint-disable-next-line max-classes-per-file
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveConfigModule } from '@deejayy/reactive-config';
+import { RuntimeLocalizerModule } from '@deejayy/runtime-localizer';
 import { environment } from '@env/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { FrameModule } from '@shared/frame/frame.module';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+export class ConfigVars {
+  public apiUrl!: string;
+}
+
 @NgModule({
+  declarations: [AppComponent],
   imports: [
+    AppRoutingModule,
     BrowserAnimationsModule,
     FrameModule,
     StoreModule.forRoot(),
     EffectsModule.forRoot(),
     StoreDevtoolsModule.instrument({
-      name: 'Angular 17 Bootstrap',
+      name: 'Angular 15 Bootstrap',
       maxAge: 200,
       logOnly: environment.production,
     }),
-    // ReactiveConfigModule.forRoot(ConfigVars, { configPath: environment.config }),
-    // RuntimeLocalizerModule.forRoot([
-    //   {
-    //     lang: 'en-US',
-    //     path: '/assets/messages/messages.en-US.json',
-    //   },
-    //   {
-    //     lang: 'hu-HU',
-    //     path: '/assets/messages/messages.hu-HU.json',
-    //   },
-    // ]),
+    ReactiveConfigModule.forRoot(ConfigVars, { configPath: environment.config }),
+    RuntimeLocalizerModule.forRoot([
+      {
+        lang: 'en-US',
+        path: '/assets/messages/messages.en-US.json',
+      },
+      {
+        lang: 'hu-HU',
+        path: '/assets/messages/messages.hu-HU.json',
+      },
+    ]),
   ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
